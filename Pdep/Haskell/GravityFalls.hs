ea766424f8934f34fd31b79297b8f2fe0f290d0e
Mateo Fernandez <mfernandezcruz@frba.utn.edu.ar>
2024-03-02 06:44:29 -0300

    Cambios finales

--- Pdep/Haskell/GravityFalls.hs
+++ Pdep/Haskell/GravityFalls.hs
@@ -1,9 +1,12 @@
 {- HLINT ignore "Redundant bracket" -}
-{- HLINT ignore "Use const" -} 
+{- HLINT ignore "Use const" -}
+{- HLINT ignore "Eta reduce" -}  
 import Text.Show.Functions
 -- ================================= Gravity Falls =================================
 
 -- ================================= 1er Parte =================================
+
+    -- ================================= Punto 1 =================================
 -- ================================= Modelado de Criaturas =================================
 data Criatura = Criatura {
     peligrosidad:: Int,
@@ -12,15 +15,15 @@ data Criatura = Criatura {
 
 siempreDetras = Criatura {
     peligrosidad = 0,
+    criterioParaDeshacer = (\_ -> False)
+}
+
+gnomos cantidad = Criatura {
+    peligrosidad = 2 ^ cantidad,
     criterioParaDeshacer = tieneSopladorDeHojas
 }
 tieneItem item = elem item . items
 tieneSopladorDeHojas = tieneItem "sopladorDeHojas"
-
-gnomos cantidad = Criatura {
-    peligrosidad = 2 ^ cantidad,
-    criterioParaDeshacer = (\_ -> False)
-}
 diezGnomos = gnomos 10
 
 
@@ -29,6 +32,11 @@ fantasma asuntoPendiente poder = Criatura {
     criterioParaDeshacer = asuntoPendiente
 }
 fantasmas = map (fantasma (tieneItem "agua bendita")) [1..10]
+
+-- casper = fantasma comprarChicle 0
+-- xds = fantasma (\_ -> True) 100
+
+fantasmaDeLaB = fantasma (\persona -> nivelExperiencia persona > 1000) 10
 
 -- ================================= Modelado de Persona =================================
 type Item = String
@@ -40,6 +48,23 @@ data Persona = Persona {
 
 mabel = Persona {
     edad = 9,
-    items = ["sopladorDeHojas"],
-    nivelExperiencia = 1
+    items = ["sopladorDeHojas", "chancho"],
+    nivelExperiencia = 100
 }
+
+-- ================================= Punto 2 =================================
+ganarExperiencia persona cantidad = 
+    persona {
+        nivelExperiencia = nivelExperiencia persona + cantidad
+    }
+
+enfrentarse persona criatura 
+    | criterioParaDeshacer criatura persona  = ganarExperiencia persona (peligrosidad criatura)
+    | otherwise = escaparse
+    where escaparse = ganarExperiencia persona 1
+
+-- ================================= Punto 3 =================================
+enfrentarTodas persona = foldl enfrentarse persona -- voy iterando cada una de las criaturas con el acumulador persona, itero a mabel n veces
+
+enfrentarse' = flip enfrentarse
+enfrentarTodos persona criaturas = foldl (.) id (map enfrentarse' criaturas) persona
