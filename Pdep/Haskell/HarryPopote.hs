6dafe5fd81b12df6cb604d84be6de13f2e0e35ba
Mateo Fernandez <mfernandezcruz@frba.utn.edu.ar>
2024-03-02 06:59:59 -0300

    Harry Popote

---
+++ Pdep/Haskell/HarryPopote.hs
@@ -0,0 +1,86 @@
+{- HLINT ignore "Redundant bracket" -}
+{- HLINT ignore "Use const" -}
+{- HLINT ignore "Eta reduce" -} 
+{- HLINT ignore "Avoid lambda" -} 
+import Text.Show.Functions
+-- ================================= Harry Potter =================================
+
+-- ================================= Punto 1 =================================
+data Mago = Mago {
+    nombre :: String,
+    edad :: Int,
+    salud :: Int,
+    hechizos :: [Hechizo]
+}
+
+data Hechizo = LagrimaFenix { cantidad :: Int }
+             | SectumSempra
+             | Obliviate { cantidadOlvidar :: Int }
+             | Confundus
+
+
+-- ================================= Punto 2 =================================
+poder :: Mago -> Int
+poder (Mago _ age hp spells) = hp + age * length spells
+
+daño :: Mago -> Hechizo -> Int
+daño _ (LagrimaFenix _) = 0
+daño mago SectumSempra = if salud mago > 10 then 10 else salud mago `div` 2
+daño _ (Obliviate _) = 0
+daño _ Confundus = 0
+
+diferenciaDePoder :: Mago -> Mago -> Int
+diferenciaDePoder mago1 mago2 = abs $ poder mago1 - poder mago2
+
+
+-- ================================= Punto 3 =================================
+data Academia = Academia {
+ magos :: [Mago],
+ examenDeIngreso :: Mago -> Bool
+}
+
+-- Saber si hay algún mago sin hechizos cuyo nombre sea “Rincenwind”
+tieneNombreYSinHechizos :: Mago -> Bool
+tieneNombreYSinHechizos (Mago "Rincenwind" _ _ []) = True
+tieneNombreYSinHechizos _ = False
+
+hayMagoSinHechizosRincenwind :: Academia -> Bool
+hayMagoSinHechizosRincenwind (Academia magos _) = any tieneNombreYSinHechizos magos
+
+-- Saber si todos los magos viejos son ñoños
+esViejoÑoño :: Mago -> Bool
+esViejoÑoño m = edad m > 50 && length (hechizos m) > 3 * edad m
+
+todosViejosSonÑoños :: Academia -> Bool
+todosViejosSonÑoños (Academia magos _) = all esViejoÑoño (filter ((>50) . edad) magos)
+
+-- Conocer la cantidad de magos de la academia que no pasarían el examen de ingreso
+magosNoPasarianExamen :: Academia -> Int
+magosNoPasarianExamen (Academia magos examen) = length (filter (not . examen) magos)
+
+
+-- ================================= Punto 4 =================================
+f _ [y] = y
+f x (y1:y2:ys)
+     | x y1 y2 = f x (y1:ys)
+     | otherwise = f x (y2 : ys)
+
+
+-- La función f parece ser una implementación de un algoritmo de búsqueda que toma una función y una lista, y retorna un valor de esa lista que cumple cierta condición definida por la función.*/
+
+mejorHechizoContra :: Mago -> Mago -> Hechizo
+mejorHechizoContra atacante defensor = f (\h1 h2 -> daño defensor h1 >= daño defensor h2) (hechizos atacante)
+
+mejorOponente :: Mago -> Academia -> Mago
+mejorOponente mago (Academia magos _) = f (\m1 m2 -> diferenciaDePoder mago m1 >= diferenciaDePoder mago m2) magos
+
+
+-- ================================= Punto 5 =================================
+noPuedeGanarle :: Mago -> Mago -> Bool
+noPuedeGanarle atacante defensor = salud defensor == salud (foldl (\m h -> aplicarHechizo h m) defensor (hechizos atacante))
+    where
+        aplicarHechizo :: Hechizo -> Mago -> Mago
+        aplicarHechizo (LagrimaFenix c) m = m { salud = salud m + c }
+        aplicarHechizo SectumSempra m = m { salud = salud m - daño m SectumSempra }
+        aplicarHechizo (Obliviate n) m = m { hechizos = drop n (hechizos m) }
+        aplicarHechizo Confundus m = m { salud = salud m - daño m Confundus }
