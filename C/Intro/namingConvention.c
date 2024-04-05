f02732cb705d2f26e8e146e353a4e380d9dd1334
Mateo <matefernandezc@gmail.com>
2024-04-05 09:48:52 -0300

    Ejercicios de C

---
+++ C/Intro/namingConvention.c
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////
+//                  Variable name examples                  //
+/////////////////////////////////////////////////////////////
+
+//          Rule 1: Cant star with digits
+int globe;
+// int 2globe;
+
+//          Rule 2: Case sensitive, this two examples are different
+int _hola; 
+int _Hola;
+
+//          Rule 3: Blankc spaces or special characters are not allowed
+// int H@ la;
+
+//          Rule 4: Cant use reserved names, an exception if you use it uppercase (rule 2)
+int Switch;
+//int switch; // Not allowed, switch is a reserved word
