d02d7e9e15e90896bf22ccf33823649de0ebf802
Mateo <matefernandezc@gmail.com>
2024-10-09 13:13:37 -0300

    Update bubbleSort.cpp

--- AyED/testing/algorithms/bubbleSort.cpp
+++ AyED/testing/algorithms/bubbleSort.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main(){
-    int dim = 15;
-    int v[dim] = {34, 76, 23, 89, 45, 12, 58, 97, 1, 42, 66, 20, 54, 31, 5};
+    int dim = 5;
+    int v[5] = {3, 2, 1, 4, 5};
     
+    // Bubble Sort
     for(int i=0; i<dim; i++){
         for(int j=0; j<dim; j++){
             if(v[j] > v[j+1]){
-                int aux = v[j];
-                v[j] = v[j+1];
-                v[j+1] = aux;
+                int aux = v[j+1];
+                v[j+1] = v[j];
+                v[j] = aux;
             }
         }
     }
