a8a2081edfee23a5b9eb8ffe6736e29cd9dbd539
Mateo <matefernandezc@gmail.com>
2024-10-16 23:03:51 -0300

    Fix gitignore

--- Pdep/Wollok/configuration/org.eclipse.osgi/516/0/.cp/icons/wollok-logo.iconset/createIco.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . -name "*.png" | grep -v 512 | grep -v 256 | xargs png2ico wollok-logo.ico
--- Pdep/Wollok/configuration/org.eclipse.osgi/516/0/.cp/icons/wollok-logo.iconset/createWindowIcnsFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(uname)" == "Darwin" ]; then
-   echo "Running on Mac..."
-   iconutil -c icns .
-else
-   echo "Running on linux..."
-   png2icns ../wollok-logo.icns icon_*.png
-fi
